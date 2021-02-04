b31526647216f5d175d356f6774f68e368fb5a71
FraCav99 <MasterCaster36@outlook.it>
2021-02-04 17:44:29 +0100

    Create templates and add new route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
+var moment = require('moment');
 var router = express.Router();
+
+// Sample messages
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: moment()
+      .startOf('hour' - 1)
+      .fromNow(),
+    title: "Hello everyone!"
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: moment()
+      .startOf('hour' - 1)
+      .fromNow(),
+    title: "Today is a good day!"
+  }
+];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Messageboard', messages });
 });
 
 module.exports = router;
